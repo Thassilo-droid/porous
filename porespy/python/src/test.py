368cd0d489e206ba7cccec9922e82e8a2e9e4672
Thassilo-droid <thassilo.horn@gmx.de>
2024-12-03 21:39:56 +0100

    porespy changes

--- porespy/python/src/test.py
+++ porespy/python/src/test.py
@@ -2,59 +2,35 @@ import os
 import numpy as np
 import porespy as ps
 from skimage.io import imread
+from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
-# Path to original image
-image_path = r'..\\..\\daten\\30ppi__rec_voi_1642.png'
+def load_image_to_array(folder_path):
+    # Get a sorted list of the PNG files in the folder
+    file_list = sorted([f for f in os.listdir(folder_path) if f.endswith('.png')])
 
-# Set image to original image
-image = imread(os.path.join(image_path))
+    # Load and threshold images (binarize) and stack them into a 3D numpy array
+    images = []
 
-# Binarize image (only True and False values)
-binary_image = image < 0.5
+    for file in file_list:
 
-# Filter binar_image with ps.filters.porosimetry filter
-filtered_image = ps.filters.porosimetry(binary_image)
+        # Load image
+        image = imread(os.path.join(folder_path, file))
 
+        # Convert to greayscale to remove 4th dimension
+        if image.ndim == 3 and image.shape[-1] == 3:
+            image = rgb2gray(image)
 
-# Calculate poresize distribution with ps.metrics.pore_size_distribution (with log = False); bins = 100 (intervals of datapoints taken)
-data = ps.metrics.pore_size_distribution(im=filtered_image, log=False, bins = 500)
+        # Threshold to binarize the image (<0.5)
+        binary_image = image < 0.5
 
-# Display data; pdf = Probability Density Function; cdf = Cumulative Density Function
-fig, ax = plt.subplots(1, 5, figsize=[20, 4])
+        # Add binarized images to images[] list
+        images.append(binary_image)
+    
+    return np.array(images)
 
-# PDF plot
-ax[0].plot(data['bin_centers'], data['pdf'], label='PDF', color='blue')
-ax[0].set_title("Probability Density Function")
-ax[0].set_xlabel("Pore radius [pixel]")
-ax[0].set_ylabel("Density")
-ax[0].legend()
-
-# CDF plot
-ax[1].plot(data['bin_centers'], data['cdf'], label='CDF', color='green')
-ax[1].set_title("Cumulative Density Function")
-ax[1].set_xlabel("Pore radius [pixel]")
-ax[1].set_ylabel("Cumulative Probability")
-ax[1].legend()
-
-# Bar plot
-ax[2].bar(data['bin_centers'], data['pdf'], width=data['bin_widths'], edgecolor='k', label='Bar Plot')
-ax[2].set_title("Bar Plot (PDF)")
-ax[2].set_xlabel("Pore radius [pixel]")
-ax[2].set_ylabel("Density")
-ax[2].legend()
-
-# Display binary image
-ax[3].imshow(((binary_image)*255).astype(np.uint8), cmap='gray')
-ax[3].set_title("Binary Image")
-ax[3].axis('off')
-
-# Display image
-ax[4].imshow(image)
-ax[4].set_title("Image")
-ax[4].axis('off')
+dataset_path = "C:\\Users\\thass\\Desktop\\non_inverted"
 
 
-plt.tight_layout()
-plt.show()
+
 
--- porespy/python/src/test2.py
+++ porespy/python/src/test2.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import porespy as ps
 from skimage.io import imread
+from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
 # Path to original image
@@ -13,29 +14,63 @@ image = imread(os.path.join(image_path))
 # Binarize image (only True and False values)
 binary_image = image < 0.5
 
-# Filter binar_image with ps.filters.porosimetry filter
-#filtered_image = ps.filters.local_thickness(binary_image)
+# grayscale
+grayscale_image = rgb2gray(binary_image)
 
 # Lokale Dicke berechnen (Porenradius)
-lt = ps.filters.local_thickness(binary_image)
+im = ps.filters.local_thickness(grayscale_image)
 
-# Umwandlung in physikalische Einheiten und Durchmesser
-voxel_size = 1  # Falls z.B. ein Voxel = 10 µm ist, setzen Sie voxel_size=10
-radii = lt[lt > 0] * voxel_size  # Porenradius in µm
-diameters = radii * 2  # Umrechnung von Radius zu Durchmesser
 
-# Benutzerdefinierte Intervalle: 0 bis 60 µm, in Schritten von 5 µm
-bin_edges = np.arange(0, 65, 5)  # 0, 5, 10, ..., 60 µm
-hist, _ = np.histogram(diameters, bins=bin_edges, density=False)
-hist_percentage = hist / hist.sum() * 100  # In Prozent umwandeln
-bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2  # Mittelpunkte der Intervalle
+print(type(im))
+print("------------------")
+print(im)
+print("------------------")
+print(im.shape)
+print("------------------")
 
-# Plotten
-plt.figure(figsize=(8, 5))
-plt.bar(bin_centers, hist_percentage, width=np.diff(bin_edges), edgecolor='k', alpha=0.7)
-plt.xlabel('Porendurchmesser (µm)')
-plt.ylabel('Wahrscheinlichkeit (%)')
-plt.title('Wahrscheinlichkeit für Porengrößen (0 bis 60 µm)')
-plt.grid(alpha=0.3)
+# Alle Porengrößen (größer als 0) extrahieren und ein Histogramm erstellen
+radii = im[im > 0].flatten()  # Alle Werte in einer 1D-Liste
+radii[145000:150000] = 57
+print(f"radii:{radii}")
+print(f"radii.shape:{radii.shape}")
+
+a = np.array([])
+
+c = 0
+
+
+for i in range(len(radii)):
+    if radii[i] >= 1 and radii[i] <= 5:
+        c+=1
+        a = np.append(a, radii[i])
+
+print(a)
+print(c)
+
+#print(np.count_nonzero(radii>1.4825 and radii < 1.5))
+
+print(f"kleinstes Element in radii:{np.min(radii)}")
+
+
+'''plt.hist(radii, bins=10, density=True, edgecolor='black')  # Bins einstellen
+plt.xlabel('Porenradius (Pixel)')
+plt.ylabel('Wahrscheinlichkeit')
+plt.title('Histogramm der Porenradien (in Pixeln)')
+plt.show()'''
+
+# Plot 1: Graustufenbild
+plt.subplot(1, 2, 1)
+plt.imshow(grayscale_image, cmap='gray')
+plt.title('Graustufenbild')
+plt.axis('off')  # Achsen ausblenden
+
+# Plot 2: Histogramm der Porengrößen
+plt.subplot(1, 2, 2)
+plt.hist(radii, bins=75, density=True, stacked=True, cumulative=True, rwidth = (1/75), edgecolor='black')  # Histogramm mit 10 Bins
+plt.xlabel('Porenradius (Pixel)')
+plt.ylabel('Wahrscheinlichkeit')
+plt.title('Histogramm der Porenradien (in Pixeln)')
+
+# Darstellung der Plots
+plt.tight_layout()
 plt.show()
-
